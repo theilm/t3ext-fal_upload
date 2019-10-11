44bb42019282208f2fdc73a04e7c5e2b67e078f1
Thomas Heilmann <1016315+theilm@users.noreply.github.com>
2019-10-11 16:45:39 +0200

    Change version constraint in ext_emconf.php

--- ext_emconf.php
+++ ext_emconf.php
@@ -10,7 +10,7 @@ $EM_CONF[$_EXTKEY] = array(
     'version' => '0.1.1',
     'constraints' => array(
         'depends' => array(
-            'typo3' => '7.5.0-8.99.99',
+            'typo3' => '7.5.0-0.99.99',
         ),
         'conflicts' => array(),
         'suggests' => array(),
